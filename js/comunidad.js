4064e8419b988ead272cbb773da9884326e6d28a
chakruk <142333026+chakruk@users.noreply.github.com>
2023-09-08 14:34:50 -0300

    Update comunidad.js

--- js/comunidad.js
+++ js/comunidad.js
@@ -2,8 +2,9 @@
 import { getUser, message } from './global.js'
 
 
-// const baseUrl = "https://test.backchakruk.quinto.site/"
-const baseUrl = "http://149.50.131.196:8080/chakruk-0.0.1-SNAPSHOT/"
+
+// const baseUrl = "http://149.50.131.196:8080/chakruk-0.0.1-SNAPSHOT/"
+const baseUrl = "/apiredirect/"
 // const baseUrl = "http://localhost:8080/"
 
 // --> Setea a la posicion inicial el paginado de comentarios al caragr la pagina
